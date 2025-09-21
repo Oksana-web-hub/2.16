1c96e4eb7ddb4163390f12a8ef1c9cd712b09181
Oksana-web-hub <oksara33@gmail.com>
2025-09-21 15:29:42 +0300

    fourth  commit

--- script.js
+++ script.js
@@ -9,21 +9,45 @@
 // console.log((hamburger && fries));
 
 
-const hamburger = 3;
-const fries = 1;
-const cola = 1;
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
 
-console.log((hamburger===3  && cola && fries))
+// console.log((hamburger===3  && cola && fries))
 
-console.log(1 && 0);
-console.log(1 && 5);
-console.log(null && 5);
-console.log(1 && 'dlkfjgvnse;ngl');
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(1 && 'dlkfjgvnse;ngl');
 
 
-if (hamburger===3  && cola===1 && fries) {
-    console.log('Ми ситі')
-} else {
-    console.log('Йдемо в інший заклад')
+// if (hamburger===3  && cola===1 && fries) {
+//     console.log('Ми ситі')
+// } else {
+//     console.log('Йдемо в інший заклад')
     
-}
+// }
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('Нас влаштовує')
+// } else {
+//     console.log('Йдемо в інший заклад')
+    
+// }
+
+
+
+
+// console.log( hamburger === 3 && cola === 2 || fries === 3 && nuggets) 
+
+// console.log(3 > 1 )
+
+
