6e7bd3074090f8568dcd0725508cf6833e8af27c
Oksana-web-hub <oksara33@gmail.com>
2025-09-21 14:13:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+
+//     console.log('Bingo');
+// }
+
+// console.log((hamburger && fries));
+
+
+const hamburger = 2;
+const fries = 1;
+
+if (hamburger===3 && fries) {
+    console.log('Ми ситі')
+} else {
+    console.log('Йдемо в інший заклад')
+    
+}
